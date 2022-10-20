a932d1040b45c8478f72c35d829e775aca64ed3c
Yarn k <3039793769@qq.com>
2022-10-20 10:56:52 +0800

    '图片验证码'

--- blog/users/urls.py
+++ blog/users/urls.py
@@ -1,7 +1,8 @@
 # Name : Yarn_k
 # Date : 2022/10/20 10:42
 from django.urls import path
-from users.views import RegisterView
+from users.views import RegisterView, ImageCodeView
+
 # from users.views import SmsCodeView, LoginView
 # from users.views import LogoutView, ForgetPasswordView
 # from users.views import UserCenterView, WriteBlogView
@@ -11,7 +12,7 @@ urlpatterns = [
     # 参数2：视图函数
     # 参数3：路由名，方便通过reverse来获取路由
     path('register/', RegisterView.as_view(), name='register'),
-    # path('imagecode/', ImageCodeView.as_view(),name='imagecode'),
+    path('imagecode/', ImageCodeView.as_view(), name='imagecode'),
     # path('smscode/', SmsCodeView.as_view(),name='smscode'),
     # path('login/', LoginView.as_view(),name='login'),
     # path('logout/', LogoutView.as_view(),name='logout'),
