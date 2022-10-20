2ac5f8853a40126bc5e94ee75e49634fb77511fd
Yarn k <3039793769@qq.com>
2022-10-20 11:04:31 +0800

    '短信验证码功能实现'

--- blog/users/urls.py
+++ blog/users/urls.py
@@ -1,7 +1,7 @@
 # Name : Yarn_k
 # Date : 2022/10/20 10:42
 from django.urls import path
-from users.views import RegisterView, ImageCodeView
+from users.views import RegisterView, ImageCodeView, SmsCodeView
 
 # from users.views import SmsCodeView, LoginView
 # from users.views import LogoutView, ForgetPasswordView
@@ -13,7 +13,7 @@ urlpatterns = [
     # 参数3：路由名，方便通过reverse来获取路由
     path('register/', RegisterView.as_view(), name='register'),
     path('imagecode/', ImageCodeView.as_view(), name='imagecode'),
-    # path('smscode/', SmsCodeView.as_view(),name='smscode'),
+    path('smscode/', SmsCodeView.as_view(), name='smscode'),
     # path('login/', LoginView.as_view(),name='login'),
     # path('logout/', LogoutView.as_view(),name='logout'),
     # path('forgetpassword/', ForgetPasswordView.as_view(),name='forgetpassword'),
